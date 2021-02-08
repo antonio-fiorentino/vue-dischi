a745458c56a3cf047866d511adf6faee0d63aa16
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-02-08 16:33:07 +0100

    Esercizio

---
+++ main.js
@@ -0,0 +1,22 @@
+// Stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+
+
+var app = new Vue({
+ el: '#root',
+ data:{
+   cdElements:[]
+ },
+
+ mounted(){
+   const self = this;
+   axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+   .then(function(element){
+     console.log(element);
+     const disks = element.data.response
+     self.cdElements = disks;
+   })
+ }
+
+});
+Vue.config.devtools = true
